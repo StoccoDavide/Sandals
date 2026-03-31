14d734114e52faf4f851db6f8b695c45b220fa34
Davide Stocco <davide.stocco@unitn.it>
2026-03-31 10:40:51 +0200

    Other changes for project reshape

--- core/tests/BoundaryValueProblems/BasicDAE.hh
+++ core/tests/BoundaryValueProblems/BasicDAE.hh
@@ -1,29 +1,29 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
- * Copyright (c) 2025, Davide Stocco and Enrico Bertolazzi.                                      *
- *                                                                                               *
- * The Sandals project is distributed under the BSD 2-Clause License.                            *
- *                                                                                               *
- * Davide Stocco                                                               Enrico Bertolazzi *
- * University of Trento                                                     University of Trento *
- * e-mail: davide.stocco@unitn.it                             e-mail: enrico.bertolazzi@unitn.it *
-\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+ * Copyright (c) 2026, Davide Stocco and Enrico Bertolazzi.                  *
+ *                                                                           *
+ * The Sandals project is distributed under the BSD 2-Clause License.        *
+ *                                                                           *
+ * Davide Stocco                                           Enrico Bertolazzi *
+ * University of Trento                                 University of Trento *
+ * davide.stocco@unitn.it                         enrico.bertolazzi@unitn.it *
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #ifndef TESTS_PROBLEMS_BASIC_DAE_HH
 #define TESTS_PROBLEMS_BASIC_DAE_HH
 
+#include "Sandals/BoundaryValueProblem.hh"
 #include "Sandals/System/Explicit.hh"
 #include "Sandals/System/Implicit.hh"
 #include "Sandals/System/SemiExplicit.hh"
-#include "Sandals/BoundaryValueProblem.hh"
 
 using namespace Sandals;
 
-//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  - - - - - - - - - -
 
-template<typename Real = double>
-class BasicDAEImplicit : public Implicit<Real, 2, 0>
-{
-public:
+template <typename Real = double>
+class BasicDAEImplicit : public Implicit<Real, 2, 0> {
+ public:
   using typename Implicit<Real, 2, 0>::VectorF;
   using typename Implicit<Real, 2, 0>::MatrixJF;
   using typename Implicit<Real, 2, 0>::VectorH;
@@ -33,44 +33,52 @@ public:
 
   ~BasicDAEImplicit() {}
 
-  VectorF F(VectorF const & x, VectorF const & x_dot, Real const /*t*/) const override
-  {
+  VectorF F(const VectorF &x,
+            const VectorF &x_dot,
+            const Real /*t*/) const override {
     VectorF F;
     F << x_dot(0) - 1.0, x(0) - x(1) + 1.0;
     return F;
   }
 
-  MatrixJF JF_x(VectorF const & /*x*/, VectorF const & /*x_dot*/, Real const /*t*/) const override {
+  MatrixJF JF_x(const VectorF & /*x*/,
+                const VectorF & /*x_dot*/,
+                const Real /*t*/) const override {
     MatrixJF JF_x(MatrixJF::Zero());
     JF_x(1, 0) = 1.0;
     JF_x(1, 1) = -1.0;
     return JF_x;
   }
 
-  MatrixJF JF_x_dot(VectorF const & /*x*/, VectorF const & /*x_dot*/, Real const /*t*/) const override {
+  MatrixJF JF_x_dot(const VectorF & /*x*/,
+                    const VectorF & /*x_dot*/,
+                    const Real /*t*/) const override {
     MatrixJF JF_x_dot(MatrixJF::Zero());
     JF_x_dot(0, 0) = 1.0;
     return JF_x_dot;
   }
 
-  VectorH h(VectorF const & /*x*/, Real const /*t*/) const override {
+  VectorH h(const VectorF & /*x*/, const Real /*t*/) const override {
     VectorH h;
     h << 0.0;
     return h;
   }
 
-  MatrixJH Jh_x(VectorF const & /*x*/, Real const /*t*/) const override {return MatrixJH::Zero();}
+  MatrixJH Jh_x(const VectorF & /*x*/, const Real /*t*/) const override {
+    return MatrixJH::Zero();
+  }
 
-  bool in_domain(VectorF const & /*x*/, Real const /*t*/) const override {return true;}
-
+  bool in_domain(const VectorF & /*x*/, const Real /*t*/) const override {
+    return true;
+  }
 };
 
-//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  - - - - - - - - - -
 
-template<typename Real, typename System, typename Integrator>
-class BasicDAEProblem : public BoundaryValueProblem<Real, 2, 0, Integrator>
-{
-public:
+template <typename Real, typename System, typename Integrator>
+class BasicDAEProblem : public BoundaryValueProblem<Real, 2, 0, Integrator> {
+ public:
   using typename BoundaryValueProblem<Real, 2, 0, Integrator>::SystemPtr;
   using typename BoundaryValueProblem<Real, 2, 0, Integrator>::IntegratorPtr;
   using typename BoundaryValueProblem<Real, 2, 0, Integrator>::SolutionPtr;
@@ -80,55 +88,66 @@ public:
   using MatrixX = Eigen::Matrix<Real, 2, Eigen::Dynamic>;
 
   BasicDAEProblem()
-    : BoundaryValueProblem<Real, 2, 0, Integrator>("BasicDAEProblem", std::make_unique<System>(), std::make_unique<Integrator>()) {}
+      : BoundaryValueProblem<Real, 2, 0, Integrator>(
+            "BasicDAEProblem",
+            std::make_unique<System>(),
+            std::make_unique<Integrator>()) {}
 
   ~BasicDAEProblem() {}
 
-  static Real time_start() {return 0.0;}
+  static Real time_start() {
+    return 0.0;
+  }
 
-  static Real time_end() {return 1.0;}
+  static Real time_end() {
+    return 1.0;
+  }
 
-  VectorF b(VectorF const & x_ini, VectorF const & x_end) const override
-  {
+  VectorF b(const VectorF &x_ini, const VectorF &x_end) const override {
     VectorF b;
     b << x_ini(0), x_end(1);
     return b;
   }
 
-  MatrixJF Jb_x_ini(VectorF const & /*x_ini*/, VectorF const & /*x_end*/) const override
-  {
+  MatrixJF Jb_x_ini(const VectorF & /*x_ini*/,
+                    const VectorF & /*x_end*/) const override {
     MatrixJF Jb_x_ini(MatrixJF::Zero());
     Jb_x_ini(0, 0) = 1.0;
     return Jb_x_ini;
   }
 
-  MatrixJF Jb_x_end(VectorF const & /*x_ini*/, VectorF const & /*x_end*/) const override
-  {
+  MatrixJF Jb_x_end(const VectorF & /*x_ini*/,
+                    const VectorF & /*x_end*/) const override {
     MatrixJF Jb_x_end(MatrixJF::Zero());
     Jb_x_end(1, 1) = 1.0;
     return Jb_x_end;
   }
 
-  static VectorF exact_solution(Real const t) {
+  static VectorF exact_solution(const Real t) {
     VectorF x;
-    x << 0.5*t*t - t, t - 1.0;
+    x << 0.5 * t * t - t, t - 1.0;
     return x;
   }
 
-  static MatrixX exact_solution(VectorX const & t) {
+  static MatrixX exact_solution(const VectorX &t) {
     MatrixX x(2, t.size());
     for (Integer i{0}; i < t.size(); ++i) {
-      x.col(i) = BasicDAEProblem<Real, System, Integrator>::exact_solution(t(i));
+      x.col(i) =
+          BasicDAEProblem<Real, System, Integrator>::exact_solution(t(i));
     }
     return x;
   }
 
-  static VectorF guess(Real const /*t*/) {return VectorF::Zero();}
+  static VectorF guess(const Real /*t*/) {
+    return VectorF::Zero();
+  }
 
-  static MatrixX guess(VectorX const & t) {return MatrixX::Zero(2, t.size());}
-
+  static MatrixX guess(const VectorX &t) {
+    return MatrixX::Zero(2, t.size());
+  }
 };
 
-//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  - - - - - - - - - -
 
-#endif // TESTS_PROBLEMS_BASIC_DAE_HH
+#endif  // TESTS_PROBLEMS_BASIC_DAE_HH
